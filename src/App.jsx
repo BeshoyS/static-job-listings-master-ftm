2f24fbeb08cf738faeb44dc7e7fa082f0d71a319
Beshoy S. Said <72396606+BeshoyS@users.noreply.github.com>
2022-12-09 21:31:23 +0100

    uploaded starter files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ListingCard from "./components/ListingCard/ListingCard";
+
+function App() {
+  return (
+    <div className="App">
+      <ListingCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListingCard/ListingCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ListingCard.module.css";
+
+const ListingCard = () => {
+  return <div className={styles.container}>ListingCard</div>;
+};
+
+export default ListingCard;
