74fe412783507d48e7448fe59ffe6896949ea8be
Beshoy S. Said <72396606+BeshoyS@users.noreply.github.com>
2022-12-10 01:13:47 +0100

    finished the listing component with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import ListingCard from "./components/ListingCard/ListingCard";
+import BannerDT from "./assets/images/bg-header-desktop.svg";
+import BannerMB from "./assets/images/bg-header-mobile.svg";
+import data from "./data/data.json";
 
 function App() {
   return (
     <div className="App">
-      <ListingCard />
+      <div className="banner">
+        <picture>
+          <source srcSet={BannerDT} media="(min-width: 900px)" />
+          <img src={BannerMB} alt="" />
+        </picture>
+      </div>
+      <h1></h1>
+      <div className="container listings">
+        {data?.map((obj) => (
+          <ListingCard key={obj.id} listing={obj} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -1,8 +1,57 @@
 import React from "react";
+import data from "../../data/data.json";
 import styles from "./ListingCard.module.css";
 
-const ListingCard = () => {
-  return <div className={styles.container}>ListingCard</div>;
+const ListingCard = ({ listing }) => {
+  const {
+    id,
+    company,
+    logo,
+    new: newBadge,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools,
+  } = listing;
+
+  return (
+    <div className={`${styles.container}`}>
+      {featured && <div className={styles["container--featured"]}></div>}
+      <div className={styles["listing-details"]}>
+        <div className={styles.logowrapper}>
+          <img src={logo} alt="" />
+        </div>
+        <div className={styles["listing-info"]}>
+          <div className={styles["title-badges"]}>
+            <p className={styles.company}>{company}</p>
+            {newBadge && <span className={styles["new-badge"]}>new!</span>}
+            {featured && (
+              <span className={styles["featured-badge"]}>featured</span>
+            )}
+          </div>
+          <h2 className={styles.position}>{position}</h2>
+          <div className={styles.demographies}>
+            <p className={styles["posted-at"]}>{postedAt}</p>
+            <p className={styles.contract}>{contract}</p>
+            <p className={styles.location}>{location}</p>
+          </div>
+        </div>
+        <div className={styles.divider}></div>
+      </div>
+      <div className={styles.tags}>
+        {[role, level, ...languages, ...tools].map((tag, index) => (
+          <button key={index} className={styles.btn}>
+            {tag}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ListingCard;
